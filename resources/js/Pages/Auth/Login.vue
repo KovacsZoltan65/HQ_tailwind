13578448248d3abcfea072891cadf29db56efdba
Kovács Zoltán <kovacs.zoltan@softc.hu>
2024-04-09 13:15:20 +0200

    email and password

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -75,6 +75,10 @@ const submit = () => {
                     <span class="ms-2 text-sm text-gray-600">Remember me</span>
                 </label>
             </div>
+            
+            <div>
+                superadmin@laraveltuts.com | password
+            </div>
 
             <div class="flex items-center justify-end mt-4">
                 <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -84,6 +88,7 @@ const submit = () => {
                 <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Log in
                 </PrimaryButton>
+                
             </div>
         </form>
     </AuthenticationCard>
